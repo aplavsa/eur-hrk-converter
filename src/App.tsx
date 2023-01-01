ef74ba15e4c73412acd672e42bea962e3f169291
Andrija Plavsa <andrijaplavsa@gmail.com>
2023-01-01 20:17:08 +0100

    NO_TICKET initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from "styled-components"
+import Converter from './Converter';
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  width: 100vw;
+  background: ${props => props.theme.secondary}; 
+
+`
+
+const HeaderTitle = styled.h1`
+  font-size: 2rem;
+  font-weight: 400;
+  color: rgb(255,255,255); 
+`
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  margin: 0;
+  padding: 1rem;
+`
+
+const MainContainer = styled.div`
+  max-width:20rem;
+  width: 100%;
+  min-height: 20rem;
+  background-color: ${props => props.theme.accentBackgroundAlt};
+  
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider
+        theme={{
+          primary: "#04080fff",
+          secondary: "#507dbcff",
+          accentBackground: "#a1c6eaff",
+          accentBackgroundAlt: "#bbd1eaff",
+          background: "#dae3e5ff",
+        }}
+      >
+        <Header>
+          <HeaderTitle>EUR Converter</HeaderTitle>
+        </Header>
+        <Main>
+          <MainContainer>
+            <Converter/>
+          </MainContainer>
+        </Main>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/Converter.tsx
@@ -0,0 +1,105 @@
+import React, { ChangeEvent, FormEventHandler, MouseEventHandler, useState } from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    margin: 0;
+    padding: 0rem;
+    width: 100%; 
+    
+`
+
+const TextContainer = styled.div`
+    padding: 0 0.5rem;
+    p {
+        font-size: 1rem;
+        font-weight: 400;
+        color: rgb(0,0,0); 
+        margin-block-start: 0;
+    }
+`
+const Container = styled.div`
+    width: auto;
+    padding: 1rem;
+    margin: 0.5rem;
+    border-radius: 0.5rem;
+    background: ${props => props.theme.secondary};
+    p {
+        margin: 0;
+        text-align: right;
+    }
+    span {
+        font-size: 2rem;
+    }
+`
+
+const TECAJ = 7.53450; 
+
+export default function Converter() {
+    const [racun, setRacun] = useState(0);
+    const [cash, setCash] = useState(0); 
+    const [toReturn, setToReturn] = useState(0); 
+    const [error, setError] = useState<string>();
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        let parsedNumber = Number.parseFloat(e.target.value)
+        if(isNaN(parsedNumber)){
+            setError("Uneseni iznosi moraju biti u brojčanoj vrijednosti, npr. 12,6");
+            
+        }
+        setError(undefined);
+        switch (e.target.name) {
+            case "amount":
+                setRacun(parsedNumber)
+                break;
+                case "cash":
+                    setCash(parsedNumber)
+                    break;
+        
+            default:
+                break;
+        }
+    }
+const calculate: FormEventHandler = (e) => {
+    e.preventDefault(); 
+    let iznosUHrk = racun * TECAJ; 
+    let ostatak = cash - iznosUHrk; 
+    if(ostatak > 0) {
+        setToReturn(ostatak / TECAJ);
+    } else {
+        setError("Nedovoljno za podmiriti račun")
+    }
+}
+
+const racunUKunama = (racun * TECAJ)
+  return (
+    <Wrapper>
+        <form onSubmit={calculate}>
+      <TextContainer>
+        <p>Unesite iznos računa u eurima</p>
+      </TextContainer>
+      <TextContainer>
+        <label htmlFor="amount">Iznos (€)</label>
+        <input name="amount" autoComplete="off" type="number" onChange={handleChange} value={racun}/>
+      </TextContainer>
+      {!isNaN(racunUKunama) && <TextContainer>
+        <p>Iznos u kunama: <span>{racunUKunama.toFixed(2)}</span></p>
+      </TextContainer>}
+      <TextContainer>
+        <label htmlFor="cash">Iznos gotovine (HRK)</label>
+        <input name="cash" autoComplete="off" type="number" onChange={handleChange} value={cash}/>
+      </TextContainer>
+      <TextContainer>
+        <span>{error}</span>
+      </TextContainer>
+      <TextContainer>
+        <button type="submit">Izračunaj</button>
+      </TextContainer>
+      <Container>
+        <p>
+          Rezultat: <span>{toReturn.toFixed(2)}</span> €
+        </p>
+      </Container>
+      </form>
+    </Wrapper>
+  );
+}
